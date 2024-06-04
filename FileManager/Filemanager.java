42baab8d384a309b30470b4a02a09996fdc7c664
Tagore <tagorenat@gmail.com>
2024-06-04 14:40:32 -0300

    Ajustes 2

---
+++ FileManager/Filemanager.java
@@ -0,0 +1,20 @@
+package FileManager;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Filemanager {
+}
