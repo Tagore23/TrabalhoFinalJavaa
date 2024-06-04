82d6c05f6ffd5bb0730c406db74af87ceb032ec3
Eduardo Zaduski <eduardozaduski@gmail.com>
2024-06-04 19:15:59 -0300

    Funcionário classe abstrata

--- Models/Funcionario.java
+++ Models/Funcionario.java
@@ -1,4 +1,30 @@
 package Models;
 
-public class Funcionario {
+abstract class Funcionario {
+
+    private String nome;
+    private double salario;
+    private int horasmensais;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public int getHorasmensais() {
+        return horasmensais;
+    }
+    public void setHorasmensais(int horasmensais) {
+        this.horasmensais = horasmensais;
+    }
+
+    public abstract void aumento();
+
 }
