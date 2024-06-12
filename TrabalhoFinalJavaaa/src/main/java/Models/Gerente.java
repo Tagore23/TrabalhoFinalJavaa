3bce5ef3efa5dd57fbfc03d58190ff096277d894
gabrielbrga <163544022+gabrielbrga@users.noreply.github.com>
2024-06-12 14:54:27 -0300

    Update Gerente.java

--- TrabalhoFinalJavaaa/src/main/java/Models/Gerente.java
+++ TrabalhoFinalJavaaa/src/main/java/Models/Gerente.java
@@ -2,13 +2,17 @@ package Models;
 
 public class Gerente extends Funcionario{
 
+    public Gerente() {
+        this.setNome("João");
+        this.setSalario(5000.00);
+        this.setHorasmensais(160);
+    }
+
     @Override
     public void verInfo() {
-        
         System.out.println("Informações do gerente:");
-
-        System.out.println("Nome: "+this.getNome());
-        System.out.println("Salario: "+this.getSalario());
-        System.out.println("Horas Mensais: "+this.getHorasmensais());
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("Salário: " + this.getSalario());
+        System.out.println("Horas Mensais: " + this.getHorasmensais());
     }
 }
